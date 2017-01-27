f5f4d0b831223927fc54b17fd95063b4756edd3f
Naziya Banu <nazinaziyabanu@gmail.com>
2017-01-27 13:24:33 -0800

    Initial restructuring

---
+++ wrangler-core/src/main/java/co/cask/wrangler/steps/Columns.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.wrangler.steps;
+
+import co.cask.wrangler.api.AbstractStep;
+import co.cask.wrangler.api.PipelineContext;
+import co.cask.wrangler.api.Record;
+import co.cask.wrangler.api.SkipRecordException;
+import co.cask.wrangler.api.StepException;
+
+import java.util.List;
+
+/**
+ * A Wrangle step for setting the columns obtained form wrangling.
+ *
+ * This step will create a copy of the input {@link Record} and clears
+ * all previous column names and add new column names.
+ */
+public class Columns extends AbstractStep {
+  // Name of the columns represented in a {@link Record}
+  private List<String> columns;
+
+  // Replaces the input {@link Record} column names.
+  boolean replaceColumnNames;
+
+  public Columns(int lineno, String detail, List<String> columns) {
+    this(lineno, detail, columns, true);
+  }
+
+  public Columns(int lineno, String detail, List<String> columns, boolean replaceColumnNames) {
+    super(lineno, detail);
+    this.replaceColumnNames = replaceColumnNames;
+    this.columns = columns;
+  }
+
+  /**
+   * Sets the new column names for the {@link Record}.
+   *
+   * @param record Input {@link Record} to be wrangled by this step.
+   * @param context Specifies the context of the pipeline.
+   * @return A newly transformed {@link Record}.
+   * @throws StepException
+   */
+  @Override
+  public Record execute(Record record, PipelineContext context) throws StepException, SkipRecordException {
+    Record r = new Record(record);
+    if (replaceColumnNames) {
+      r.clearColumns();
+    }
+    for (String name : columns) {
+      r.addColumn(name);
+    }
+    return r;
+  }
+}
+
---
+++ wrangler-core/src/main/java/co/cask/wrangler/steps/Drop.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.wrangler.steps;
+
+import co.cask.wrangler.api.AbstractStep;
+import co.cask.wrangler.api.PipelineContext;
+import co.cask.wrangler.api.Record;
+import co.cask.wrangler.api.SkipRecordException;
+import co.cask.wrangler.api.StepException;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A Wrangle step for dropping the columns obtained form wrangling.
+ *
+ * This step will create a copy of the input {@link Record} and clears
+ * all previous column names and add new column names.
+ */
+public class Drop extends AbstractStep {
+  // Columns to be dropped.
+  private List<String> columns;
+
+  public Drop(int lineno, String detail, String column) {
+    this(lineno, detail, Arrays.asList(column));
+  }
+
+  public Drop(int lineno, String detail, List<String> columns) {
+    super(lineno, detail);
+    this.columns = columns;
+  }
+
+  /**
+   * Drops the columns specified.
+   *
+   * @param record Input {@link Record} to be wrangled by this step.
+   * @param context Specifies the context of the pipeline.
+   * @return A newly transformed {@link Record}.
+   * @throws StepException
+   */
+  @Override
+  public Record execute(Record record, PipelineContext context) throws StepException, SkipRecordException {
+    for(String column : columns) {
+      int idx = record.find(column);
+      if (idx != -1) {
+        record.remove(idx);
+      } else {
+        throw new StepException(toString() + " : " +
+                                  column + " column is not defined. Please check the wrangling steps.");
+      }
+    }
+    return record;
+  }
+}
+
