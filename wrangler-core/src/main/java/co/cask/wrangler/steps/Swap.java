04c271ffcb7d940c07e2d2a026aa323beeb78dca
Naziya Banu <nazinaziyabanu@gmail.com>
2017-02-01 02:11:31 -0800

    Swap directive for swapping column names

---
+++ wrangler-core/src/main/java/co/cask/wrangler/steps/Swap.java
@@ -0,0 +1,7 @@
+package co.cask.wrangler.steps;
+
+/**
+ * Created by nitin on 2/1/17.
+ */
+public class Swap {
+}
---
+++ wrangler-core/src/test/java/co/cask/wrangler/steps/SwapTest.java
@@ -0,0 +1,10 @@
+package co.cask.wrangler.steps;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nitin on 2/1/17.
+ */
+public class SwapTest {
+
+}
