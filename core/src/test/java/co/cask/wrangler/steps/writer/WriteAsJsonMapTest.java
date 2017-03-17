802045c36f926192ae63f4b0347cd05a0fe73a27
Naziya Banu <nazinaziyabanu@gmail.com>
2017-03-16 17:30:38 -0700

    Merge release/1.2 to develop (#62)
    * Fix json generation script
    * Update pom.xml
    * Change the output format of 'write-to-json-map' directive.
    * Update the script for child pom too
    * json generation script only in tranform pom
    * Move json generation script to tranform pom
    * Improve WriteAsJsonMapTest.
    * Bump versions to work with CDAP 4.2.0-SNAPSHOT

--- core/src/test/java/co/cask/wrangler/steps/writer/WriteAsJsonMapTest.java
+++ core/src/test/java/co/cask/wrangler/steps/writer/WriteAsJsonMapTest.java
@@ -16,37 +16,59 @@
 
 package co.cask.wrangler.steps.writer;
 
+import co.cask.cdap.internal.guava.reflect.TypeToken;
 import co.cask.wrangler.api.Record;
 import co.cask.wrangler.steps.PipelineTest;
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Tests {@link WriteAsJsonMap}
  */
 public class WriteAsJsonMapTest {
 
+  private static final Gson GSON = new Gson();
+
   @Test
   public void testWriteToJson() throws Exception {
     String[] directives = new String[] {
       "write-as-json-map test",
+      "keep test"
     };
 
     JSONObject o = new JSONObject();
     o.put("a", 1);
     o.put("b", "2");
+    String url = "http://www.yahoo.com?a=b c&b=ab&xyz=1";
     List<Record> records = Arrays.asList(
-      new Record("url", "http://www.yahoo.com?a=b c&b=ab&xyz=1")
+      new Record().add("int", 1).add("string", "this is string"),
+      new Record("url", url)
       .add("o", o)
-      .add("i1", new Integer(1))
-      .add("i2", new Double(1.8f))
+      .add("i1", 1)
+      .add("i2", (double) 1.8f)
     );
     records = PipelineTest.execute(directives, records);
 
-    Assert.assertTrue(records.size() == 1);
+    Assert.assertTrue(records.size() == 2);
+    Type stringStringMapType = new TypeToken<Map<String, String>>() { }.getType();
+    Map<String, String> map = GSON.fromJson((String) records.get(0).getValue("test"), stringStringMapType);
+    Assert.assertEquals(ImmutableMap.of("string", "this is string", "int", "1"), map);
+
+    JsonObject jsonObject = new JsonParser().parse((String) records.get(1).getValue("test")).getAsJsonObject();
+    Assert.assertEquals(1, jsonObject.get("i1").getAsInt());
+
+    Assert.assertEquals(1.8f, jsonObject.get("i2").getAsFloat(), 0.001);
+    Assert.assertEquals(url, jsonObject.get("url").getAsString());
+    Assert.assertEquals(GSON.toJson(o), jsonObject.get("o").toString());
   }
 }
