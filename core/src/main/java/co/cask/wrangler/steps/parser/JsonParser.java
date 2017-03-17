45e8b91d3bb371544813894a47606ab95a531116
Naziya Banu <nazinaziyabanu@gmail.com>
2017-03-16 17:32:04 -0700

    Fix lookup in Transform. (#60)

--- core/src/main/java/co/cask/wrangler/steps/parser/JsonParser.java
+++ core/src/main/java/co/cask/wrangler/steps/parser/JsonParser.java
@@ -36,7 +36,7 @@ import java.util.List;
  */
 @Usage(
   directive = "parse-as-json",
-  usage = "parse-as-json <column> <delete-column>",
+  usage = "parse-as-json <column> <depth>",
   description = "Parses a column as JSON."
 )
 public class JsonParser extends AbstractStep {
