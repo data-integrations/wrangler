2b4e395b2dd34e9c0cba3747e500d1f2fecdc9d7
Nitin Motgi <nitinmotgi@users.noreply.github.com>
2017-05-12 23:20:51 +0530

    APIs for managing connections (#96)
    * APIs for managing connections
    
    * Update connections.md
    
    * Update connections.md
    
    * Update connections.md
    
    Edits.
    
    * Update connections.md
    
    Added description as a field.
    
    * Update connections.md
    
    Removed a comma.
    
    * Update connections.md
    
    Add spaces.
    
    * Changing the type of the value
    
    * Update connections.md
    
    * Update connections.md
    
    * Update connections.md
    
    * Update connections.md
    
    * Fixing issue with key generation and type erasure
    
    * Update connections.md
    
    * Update connections.md
    
    * Update connections.md
    
    * Create connection-properties.md
    
    * Update connection-properties.md
    
    * removing unwanted files
    
    * removing unwanted files
    
    * PR comments addressed

---
+++ service/src/test/java/co/cask/wrangler/dataset/connections/ConnectionsDatasetTest.java
@@ -0,0 +1,37 @@
+package co.cask.wrangler.dataset.connections;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Class description here.
+ */
+public class ConnectionsDatasetTest {
+
+  @Test
+  public void testEncryptionAndDecryption() throws Exception {
+    try
+    {
+      String text = "Hello World";
+      String key = "Bar12345Bar12345"; // 128 bit key
+      // Create key and cipher
+      Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+      Cipher cipher = Cipher.getInstance("AES");
+      // encrypt the text
+      cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+      byte[] encrypted = cipher.doFinal(text.getBytes());
+      // decrypt the text
+      cipher.init(Cipher.DECRYPT_MODE, aesKey);
+      String decrypted = new String(cipher.doFinal(encrypted));
+      Assert.assertEquals(text, decrypted);
+    }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
