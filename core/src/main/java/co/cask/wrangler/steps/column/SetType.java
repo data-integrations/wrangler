fe07e04a7544a88ad0007a0b03e0fc3f50498e92
Naziya Banu <nazinaziyabanu@gmail.com>
2017-06-08 08:31:14 -0700

    Adding more types

--- core/src/main/java/co/cask/wrangler/steps/column/SetType.java
+++ core/src/main/java/co/cask/wrangler/steps/column/SetType.java
@@ -16,6 +16,7 @@
 
 package co.cask.wrangler.steps.column;
 
+import co.cask.cdap.api.common.Bytes;
 import co.cask.wrangler.api.AbstractStep;
 import co.cask.wrangler.api.PipelineContext;
 import co.cask.wrangler.api.Record;
@@ -29,9 +30,9 @@ import java.util.List;
  * Accepted types are: int, double, string and boolean
  */
 @Usage(
-        directive = "set-type",
-        usage = "set-type <column> <int|double|string|boolean>",
-        description = "Converting data type of a column"
+  directive = "set-type",
+  usage = "set-type <column> <int|double|string|boolean>",
+  description = "Converting data type of a column"
 )
 public class SetType extends AbstractStep {
   // Columns of the column to be upper-cased
@@ -50,106 +51,195 @@ public class SetType extends AbstractStep {
       int idx = record.find(col);
       if (idx != -1) {
         Object object = record.getValue(idx);
-
-        //convert to int
-        if (type.equals("int")) {
-          if (object instanceof String) {
-            if (object != null) {
-              String value = (String) object;
-              record.setValue(idx, Integer.valueOf(value));
-            }
-          }
-          else if (object instanceof Double) {
-            if (object != null) {
-              Double value = (Double) object;
-              record.setValue(idx, value.intValue());
-            }
-          }
-          else if (object instanceof Boolean) {
-            if (object != null) {
-              Boolean value = (Boolean) object;
-              if (value.equals(true)) {
-                record.setValue(idx, 1);
-              }
-              else {
-                record.setValue(idx, 0);
-              }
-            }
-          }
+        if (object == null) {
+          continue;
         }
-
-        //convert to string
-        if (type.equals("string")) {
-          if (object != null) {
-            record.setValue(idx, object.toString());
-          }
-        }
-
-        //convert to double
-        if (type.equals("double")) {
-          if (object instanceof Integer) {
-            if (object != null) {
-              Integer value = (Integer) object;
-              record.setValue(idx, value.doubleValue());
-            }
-          }
-          if (object instanceof String) {
-            if (object != null) {
-              String value = (String) object;
-              record.setValue(idx, Double.valueOf(value));
-            }
-          }
-          if (object instanceof Boolean) {
-            if (object != null) {
-              Boolean value = (Boolean) object;
-              if (value.equals(true)) {
-                record.setValue(idx, 1.0);
-              }
-              else {
-                record.setValue(idx, 0.0);
-              }
-            }
-          }
-        }
-
-        //convert to boolean
-        if (type.equals("boolean")) {
-          if (object instanceof Integer) {
-            if (object != null) {
-              Integer value = (Integer) object;
-              if (value.equals(0)) {
-                record.setValue(idx, false);
-              }
-              else {
-                record.setValue(idx, true);
-              }
-            }
-          }
-          if (object instanceof Double) {
-            if (object != null) {
-              Double value = (Double) object;
-              if (value.equals(0.0)) {
-                record.setValue(idx, false);
-              }
-              else {
-                record.setValue(idx, true);
-              }
-            }
-          }
-          if (object instanceof String) {
-            if (object != null) {
-              String value = (String) object;
-              if (value.equals("true")) {
-                record.setValue(idx, true);
-              }
-              else if (value.equals("false")) {
-                record.setValue(idx, false);
-              }
-            }
-          }
+        try {
+          record.setValue(idx, convertType(type, object));
+        } catch (Exception e) {
+          throw new StepException(
+            String.format(toString() + ":" + e.getMessage())
+          );
         }
       }
     }
     return records;
+  }
+
+  private Object convertType(String toType, Object object) throws Exception {
+    toType = toType.toLowerCase();
+    switch (toType) {
+      case "INTEGER":
+      case "I64":
+      case "INT": {
+        if(object instanceof String) {
+          return Integer.parseInt((String) object);
+        } else if (object instanceof Short) {
+          return ((Short) object).intValue();
+        } else if (object instanceof Float) {
+          return ((Float) object).intValue();
+        } else if (object instanceof Double) {
+          return ((Double) object).intValue();
+        } else if (object instanceof Number) {
+          return ((Double) object).intValue();
+        } else if (object instanceof Integer) {
+          return object;
+        } else if (object instanceof Long) {
+          return ((Long) object).intValue();
+        } else if (object instanceof byte[]) {
+          return Bytes.toInt((byte[]) object);
+        }
+      }
+      break;
+
+      case "I32":
+      case "SHORT": {
+        if(object instanceof String) {
+          return Short.parseShort((String) object);
+        } else if (object instanceof Short) {
+          return object;
+        } else if (object instanceof Float) {
+          return ((Float) object).shortValue();
+        } else if (object instanceof Double) {
+          return ((Double) object).shortValue();
+        } else if (object instanceof Number) {
+          return ((Double) object).shortValue();
+        } else if (object instanceof Integer) {
+          return ((Integer) object).shortValue();
+        } else if (object instanceof Long) {
+          return ((Long) object).shortValue();
+        } else if (object instanceof byte[]) {
+          return Bytes.toShort((byte[]) object);
+        }
+      }
+      break;
+
+      case "LONG": {
+        if(object instanceof String) {
+          return Long.parseLong((String) object);
+        } else if (object instanceof Short) {
+          return ((Short) object).longValue();
+        } else if (object instanceof Float) {
+          return ((Float) object).longValue();
+        } else if (object instanceof Double) {
+          return ((Double) object).longValue();
+        } else if (object instanceof Number) {
+          return ((Double) object).longValue();
+        } else if (object instanceof Integer) {
+          return ((Integer) object).longValue();
+        } else if (object instanceof Long) {
+          return object;
+        } else if (object instanceof byte[]) {
+          return Bytes.toLong((byte[]) object);
+        }
+      }
+      break;
+
+      case "BOOL":
+      case "BOOLEAN": {
+        if(object instanceof String) {
+          return Boolean.parseBoolean((String) object);
+        } else if (object instanceof Short) {
+          return ((Short) object) > 0 ? true : false;
+        } else if (object instanceof Float) {
+          return ((Float) object) > 0 ? true : false;
+        } else if (object instanceof Double) {
+          return ((Double) object)  > 0 ? true : false;
+        } else if (object instanceof Number) {
+          return ((Double) object)  > 0 ? true : false;
+        } else if (object instanceof Integer) {
+          return ((Integer) object)  > 0 ? true : false;
+        } else if (object instanceof Long) {
+          return ((Long) object)  > 0 ? true : false;
+        } else if (object instanceof byte[]) {
+          return Bytes.toBoolean((byte[]) object);
+        }
+      }
+      break;
+
+      case "STRING": {
+        if(object instanceof String) {
+          return object;
+        } else if (object instanceof Short) {
+          return Short.toString((Short) object);
+        } else if (object instanceof Float) {
+          return Float.toString((Float) object);
+        } else if (object instanceof Double) {
+          return Double.toString((Double) object);
+        } else if (object instanceof Integer) {
+          return Integer.toString((Integer) object);
+        } else if (object instanceof Long) {
+          return Long.toString((Long) object);
+        } else if (object instanceof byte[]) {
+          return Bytes.toString((byte[]) object);
+        }
+      }
+      break;
+
+      case "FLOAT": {
+        if(object instanceof String) {
+          return Float.parseFloat((String) object);
+        } else if (object instanceof Short) {
+          return ((Short) object).floatValue();
+        } else if (object instanceof Float) {
+          return object;
+        } else if (object instanceof Double) {
+          return ((Double) object).floatValue();
+        } else if (object instanceof Number) {
+          return ((Double) object).floatValue();
+        } else if (object instanceof Integer) {
+          return ((Integer) object).floatValue();
+        } else if (object instanceof Long) {
+          return ((Long) object).longValue();
+        } else if (object instanceof byte[]) {
+          return Bytes.toFloat((byte[]) object);
+        }
+      }
+      break;
+
+      case "DOUBLE": {
+        if(object instanceof String) {
+          return Double.parseDouble((String) object);
+        } else if (object instanceof Short) {
+          return ((Short) object).doubleValue();
+        } else if (object instanceof Float) {
+          return ((Float) object).doubleValue();
+        } else if (object instanceof Double) {
+          return object;
+        } else if (object instanceof Number) {
+          return ((Double) object).doubleValue();
+        } else if (object instanceof Integer) {
+          return ((Integer) object).doubleValue();
+        } else if (object instanceof Long) {
+          return ((Long) object).doubleValue();
+        } else if (object instanceof byte[]) {
+          return Bytes.toDouble((byte[]) object);
+        }
+      }
+      break;
+
+      case "BYTES": {
+        if(object instanceof String) {
+          return Bytes.toBytes((String) object);
+        } else if (object instanceof Short) {
+          return Bytes.toBytes((Short) object);
+        } else if (object instanceof Float) {
+          return Bytes.toBytes((Float) object);
+        } else if (object instanceof Double) {
+          return Bytes.toBytes((Double) object);
+        } else if (object instanceof Number) {
+          return Bytes.toBytes((Double) object);
+        } else if (object instanceof Integer) {
+          return Bytes.toBytes((Integer) object);
+        } else if (object instanceof Long) {
+          return Bytes.toBytes((Long) object);
+        } else if (object instanceof byte[]) {
+          return object;
+        }
+      }
+      break;
+
+    }
+
   }
 }
