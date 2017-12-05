7f392b57b23a1c20f105dc58be845add047403bb
Naziya Banu <nazinaziyabanu@gmail.com>
2017-12-04 23:55:46 -0800

    start bigquery

---
+++ wrangler-service/src/main/java/co/cask/wrangler/service/bigquery/BigQueryService.java
@@ -0,0 +1,49 @@
+package co.cask.wrangler.service.bigquery;
+
+import co.cask.cdap.api.annotation.UseDataSet;
+import co.cask.cdap.api.dataset.table.Table;
+import co.cask.cdap.api.service.http.AbstractHttpServiceHandler;
+import co.cask.cdap.api.service.http.HttpServiceContext;
+import co.cask.cdap.api.service.http.HttpServiceRequest;
+import co.cask.cdap.api.service.http.HttpServiceResponder;
+import co.cask.wrangler.DataPrep;
+import co.cask.wrangler.dataset.connections.ConnectionStore;
+import com.google.api.gax.paging.Page;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.Dataset;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+
+public class BigQueryService extends AbstractHttpServiceHandler {
+  private ConnectionStore store;
+  private BigQuery bigquery;
+
+  @UseDataSet(DataPrep.DATAPREP_DATASET)
+  private Table connectionTable;
+
+  @Override
+  public void initialize(HttpServiceContext context) throws Exception {
+    super.initialize(context);
+    store = new ConnectionStore(connectionTable);
+    bigquery = BigQueryOptions.getDefaultInstance().getService();
+  }
+
+  /**
+   * List all kafka topics.
+   *
+   * @param request HTTP requets handler.
+   * @param responder HTTP response handler.
+   */
+  @GET
+  @Path("connections/{connection-id}/bigquery")
+  public void list(HttpServiceRequest request, HttpServiceResponder responder) {
+    Page<Dataset> datasets = bigquery.listDatasets(BigQuery.DatasetListOption.all());
+    for (Dataset dataset : datasets.iterateAll()) {
+      dataset.list()
+    }
+  }
+}
