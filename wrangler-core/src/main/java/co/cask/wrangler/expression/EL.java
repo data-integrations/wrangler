320ad41ab5a14877ec094bceab96fcb81f753370
Naziya Banu <nazinaziyabanu@gmail.com>
2017-12-13 11:59:17 -0800

    Fix compilation.

--- wrangler-core/src/main/java/co/cask/wrangler/expression/EL.java
+++ wrangler-core/src/main/java/co/cask/wrangler/expression/EL.java
@@ -13,7 +13,7 @@
  *  License for the specific language governing permissions and limitations under
  *  the License.
  */
-s
+
 package co.cask.wrangler.expression;
 
 import co.cask.cdap.api.common.Bytes;
