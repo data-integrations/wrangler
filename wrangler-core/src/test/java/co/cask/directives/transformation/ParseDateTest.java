ee6e3ff0bb58fa58078dd2d390eedc52ee71f22f
vinisha <vinishashah@google.com>
2018-08-30 23:25:41 -0700

    CDAP-14244 parse-as-simple-date: Change Date to ZonedDateTime

--- wrangler-core/src/test/java/co/cask/directives/transformation/ParseDateTest.java
+++ wrangler-core/src/test/java/co/cask/directives/transformation/ParseDateTest.java
@@ -22,6 +22,10 @@ import co.cask.wrangler.api.Row;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -60,6 +64,85 @@ public class ParseDateTest {
     Assert.assertEquals(2678460000L, rows.get(1).getValue("difference"));
     Assert.assertEquals(0L, rows.get(2).getValue("difference"));
     Assert.assertTrue(rows.size() == 3);
+  }
+
+  @Test
+  public void testSimpleDate() throws Exception {
+    String[] directives = new String[] {
+      "parse-as-simple-date date1 MM/dd/yyyy",
+      "parse-as-simple-date date2 dd/MM/yyyy",
+      "parse-as-simple-date date3 MM-dd-yyyy",
+      "parse-as-simple-date date4 MM-dd-yy",
+      "parse-as-simple-date date5 yyyy-MM-dd",
+      "parse-as-simple-date date6 yyyy-MM-dd HH:mm:ss",
+      "parse-as-simple-date date7 MM-dd-yyyy 'at' HH:mm:ss z",
+      "parse-as-simple-date date8 dd/MM/yy HH:mm:ss",
+      "parse-as-simple-date date9 yyyy,MM.dd'T'HH:mm:ss.SSSZ",
+      "parse-as-simple-date date10 MM.dd.yyyy HH:mm:ss.SSS",
+      "parse-as-simple-date date11 EEE, d MMM yyyy HH:mm:ss",
+      "parse-as-simple-date date12 EEE, MMM d, ''yy",
+      "parse-as-simple-date date13 h:mm a",
+      "parse-as-simple-date date14 K:mm a, z",
+      "parse-as-simple-date date15 yyyy.MM.dd G 'at' HH:mm:ss z",
+    };
+
+    Row row1 = new Row();
+    // MM/dd/yyyy
+    row1.add("date1", "12/10/2016");
+    // dd/MM/yyyy
+    row1.add("date2", "10/12/2016");
+    // MM-dd-yyyy
+    row1.add("date3", "12-10-2016");
+    // MM-dd-yy
+    row1.add("date4", "12-10-16");
+    // yyyy-MM-dd
+    row1.add("date5", "2016-12-10");
+    // yyyy-MM-dd HH:mm:ss
+    row1.add("date6", "2016-12-10 06:45:11");
+    // MM-dd-yyyy 'at' HH:mm:ss with timezone
+    row1.add("date7", "12-10-2016 at 06:45:11 PST");
+    // dd/MM/yy HH:mm:ss
+    row1.add("date8", "10/12/2016 06:45:11");
+    // yyyy,MM.dd'T'HH:mm:ss.SSS with RFC timezone
+    row1.add("date9", "2016,12.10T06:45:11.111-0800");
+    // MM.dd.yyyy HH:mm:ss.SSS
+    row1.add("date10", "12.10.2016 06:45:11.111");
+    // EEE, d MMM yyyy HH:mm:ss
+    row1.add("date11", "Sat, 10 Dec 2016 06:45:11");
+    // EEE, MMM d, 'yy
+    row1.add("date12", "Sat, Dec 10, '16");
+    // h:mm AM/PM
+    row1.add("date13", "06:45 PM");
+    // H:mm with timezone
+    row1.add("date14", "06:45 PM, PST");
+    // Custom - yyyy.MM.dd G 'at' HH:mm:ss z
+    row1.add("date15", "2016.12.10 AD at 06:45:11 PST");
+
+    List<Row> rows = TestingRig.execute(directives, Arrays.asList(row1));
+    LocalDate localDate = LocalDate.of(2016, 12, 10);
+    LocalTime zeroTime = LocalTime.of(0, 0);
+    ZonedDateTime zonedDateZeroTime = ZonedDateTime.of(localDate, zeroTime, ZoneId.systemDefault());
+
+    Assert.assertEquals(zonedDateZeroTime, rows.get(0).getValue("date1"));
+    Assert.assertEquals(zonedDateZeroTime, rows.get(0).getValue("date2"));
+    Assert.assertEquals(zonedDateZeroTime, rows.get(0).getValue("date3"));
+    Assert.assertEquals(zonedDateZeroTime, rows.get(0).getValue("date4"));
+    Assert.assertEquals(zonedDateZeroTime, rows.get(0).getValue("date5"));
+
+    LocalTime localTime = LocalTime.of(6, 45, 11);
+    ZonedDateTime zonedDateTime = ZonedDateTime.of(localDate, localTime, ZoneId.systemDefault());
+    Assert.assertEquals(zonedDateTime, rows.get(0).getValue("date6"));
+    Assert.assertEquals(zonedDateTime, rows.get(0).getValue("date7"));
+    Assert.assertEquals(zonedDateTime, rows.get(0).getValue("date8"));
+    Assert.assertEquals(zonedDateTime.plusNanos(TimeUnit.SECONDS.toMicros(111)), rows.get(0).getValue("date9"));
+    Assert.assertEquals(zonedDateTime.plusNanos(TimeUnit.SECONDS.toMicros(111)), rows.get(0).getValue("date10"));
+    Assert.assertEquals(zonedDateTime, rows.get(0).getValue("date11"));
+    Assert.assertEquals(zonedDateZeroTime, rows.get(0).getValue("date12"));
+    Assert.assertEquals(ZonedDateTime.of(LocalDate.of(1970, 1, 1), LocalTime.of(18, 45), ZoneId.systemDefault()),
+                        rows.get(0).getValue("date13"));
+    Assert.assertEquals(ZonedDateTime.of(LocalDate.of(1970, 1, 1), LocalTime.of(18, 45), ZoneId.systemDefault()),
+                        rows.get(0).getValue("date14"));
+    Assert.assertEquals(zonedDateTime, rows.get(0).getValue("date15"));
   }
 
   @Test
