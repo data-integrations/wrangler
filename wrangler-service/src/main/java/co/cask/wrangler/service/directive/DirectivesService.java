932a2a9929c5b7d5b7dfbe74858181bc8b6b34da
vinisha <vinishashah@google.com>
2018-09-06 18:06:53 -0700

    CDAP-14244 Date/Time support in Spanner service

--- wrangler-service/src/main/java/co/cask/wrangler/service/directive/DirectivesService.java
+++ wrangler-service/src/main/java/co/cask/wrangler/service/directive/DirectivesService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017 Cask Data, Inc.
+ * Copyright © 2017-2018 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -641,7 +641,7 @@ public class DirectivesService extends AbstractHttpServiceHandler {
           Object object = field.getSecond();
 
           if (object != null) {
-            types.addProperty(field.getFirst(), object.getClass().getSimpleName().toLowerCase());
+            types.addProperty(field.getFirst(), object.getClass().getSimpleName());
             if ((object.getClass().getMethod("toString").getDeclaringClass() != Object.class)) {
               value.addProperty(field.getFirst(), object.toString());
             } else {
