e89cb141fd11b115227138fe2449066b0e9e9d1f
Naziya Banu <nazinaziyabanu@gmail.com>
2019-08-20 17:32:09 -0400

    Update variable name.

--- wrangler-api/src/main/java/io/cdap/wrangler/api/DirectiveParseException.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/DirectiveParseException.java
@@ -31,8 +31,8 @@ public class DirectiveParseException extends Exception {
     this.errors = errors;
   }
 
-  public DirectiveParseException(String s, Throwable e) {
-    super(s, e);
+  public DirectiveParseException(String message, Throwable e) {
+    super(message, e);
   }
 
   public DirectiveParseException(Throwable e) {
