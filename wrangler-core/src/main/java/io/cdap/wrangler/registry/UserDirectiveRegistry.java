a0c608247badf20023789c87d83462a1f2ec3b23
Nitin Motgi <nmotgi@google.com>
2020-05-10 13:55:42 -0700

    Modified error message to have proper english

--- wrangler-core/src/main/java/io/cdap/wrangler/registry/UserDirectiveRegistry.java
+++ wrangler-core/src/main/java/io/cdap/wrangler/registry/UserDirectiveRegistry.java
@@ -129,8 +129,8 @@ public final class UserDirectiveRegistry implements DirectiveRegistry {
       }
       if (directive == null) {
         throw new DirectiveLoadException(
-          String.format("Directive '%s' not found. Check if directive is spelled corrected. if the directive is " +
-                          "user defined, make sure artifact has been uploaded", name)
+          String.format("Directive '%s' not found. Check if directive is spelled corrected. If the directive is " +
+                          "user defined, make sure its artifact has been uploaded.", name)
         );
       }
       DirectiveInfo directiveInfo = new DirectiveInfo(DirectiveInfo.Scope.USER, directive);
