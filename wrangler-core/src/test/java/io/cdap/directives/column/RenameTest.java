f216d8f664b4b2b97da6646e52b179b7efadb9d7
yaojie <yaojie@google.com>
2021-02-09 01:14:58 -0800

    CDAP-16316 fix case insensitive find

--- wrangler-core/src/test/java/io/cdap/directives/column/RenameTest.java
+++ wrangler-core/src/test/java/io/cdap/directives/column/RenameTest.java
@@ -19,6 +19,7 @@ package io.cdap.directives.column;
 import io.cdap.wrangler.TestingRig;
 import io.cdap.wrangler.api.RecipeException;
 import io.cdap.wrangler.api.Row;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -40,5 +41,40 @@ public class RenameTest {
     );
 
     TestingRig.execute(directives, rows);
+  }
+
+  @Test (expected = RecipeException.class)
+  public void testRenameCaseSensitiveFailure() throws Exception {
+    String[] directives = new String[] {
+      "rename C1 c4",
+    };
+
+    List<Row> rows = Arrays.asList(
+      new Row("C1", "A").add("C2", "B").add("C3", "C").add("C4", "D").add("C5", "E")
+    );
+
+    TestingRig.execute(directives, rows);
+  }
+
+  @Test
+  public void testRenameCaseSensitiveSuccess() throws Exception {
+    String[] directives = new String[] {
+      "rename C1 c1",
+    };
+
+    List<Row> rows = Arrays.asList(
+      new Row("C1", "A").add("C2", "B").add("C3", "C").add("C4", "D").add("C5", "E")
+    );
+
+    rows = TestingRig.execute(directives, rows);
+    Assert.assertEquals(1, rows.size());
+    Row row = rows.get(0);
+    // here compare the exact string since row is case insensitive
+    Assert.assertEquals("c1", row.getFields().get(0).getFirst());
+    Assert.assertEquals("A", row.getValue("c1"));
+    Assert.assertEquals("B", row.getValue("C2"));
+    Assert.assertEquals("C", row.getValue("C3"));
+    Assert.assertEquals("D", row.getValue("C4"));
+    Assert.assertEquals("E", row.getValue("C5"));
   }
 }
