71629962c3de0b44babdb6b31c9d8f6c9034dc80
Naziya Banu <nazinaziyabanu@gmail.com>
2021-06-18 15:06:08 +0200

    don't try to decode the field if he has null value (and is nullable)

--- wrangler-core/src/main/java/io/cdap/wrangler/utils/RecordConvertor.java
+++ wrangler-core/src/main/java/io/cdap/wrangler/utils/RecordConvertor.java
@@ -111,16 +111,22 @@ public final class RecordConvertor implements Serializable {
   }
 
   private Object decode(String name, Object object, Schema schema) throws RecordConvertorException {
+    boolean isNullable = schema.isNullable();
+
+    if (object == null && isNullable) {
+      return null;
+    }
+
     // Extract the type of the field.
     Schema.Type type = schema.getType();
     Schema.LogicalType logicalType =
-        schema.isNullable() ? schema.getNonNullable().getLogicalType() :
+            isNullable ? schema.getNonNullable().getLogicalType() :
             schema.getLogicalType();
 
     if (logicalType != null) {
       switch (logicalType) {
         case DATETIME:
-          if (schema.isNullable() && object == null || object instanceof LocalDateTime) {
+          if (isNullable && object == null || object instanceof LocalDateTime) {
             return object;
           }
           if (object == null) {
