7df0e20876a83d4b8a91bddd7ffe23d881d147a2
Venkata Aravind Ginjupalli <164623835+aravind14e@users.noreply.github.com>
2025-04-12 17:27:39 +0530

    Create MockExecutorContext.java

---
+++ wrangler-core/src/test/java/io/cdap/wrangler/parser/MockExecutorContext.java
@@ -0,0 +1,13 @@
+package io.cdap.wrangler.parser;
+
+
+import io.cdap.wrangler.api.ExecutorContext;
+import org.apache.spark.util.kvstore.InMemoryStore;
+import org.reflections.Store;
+
+public class MockExecutorContext implements ExecutorContext {
+        private final Store store = new InMemoryStore();
+
+        @Override public Store getStore() { return store; }
+        // Implement other methods as no-ops if needed
+    }
