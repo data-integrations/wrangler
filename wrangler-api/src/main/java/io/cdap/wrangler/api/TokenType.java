b750f87e6af1ad0ccfbb3dc4f06ed3cdb5a18c96
Venkata Aravind Ginjupalli <164623835+aravind14e@users.noreply.github.com>
2025-04-12 16:56:27 +0530

    Create TokenType.java

---
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/TokenType.java
@@ -0,0 +1,11 @@
+@@ -152,5 +152,9 @@ public enum TokenType implements Serializable {
+   * Represents the enumerated type for the object of type {@code String} with restrictions
+   * on characters that can be present in a string.
+   */
+  IDENTIFIER
+  IDENTIFIER,
+
+  BYTE_SIZE,
+
+  TIME_DURATION
+}
