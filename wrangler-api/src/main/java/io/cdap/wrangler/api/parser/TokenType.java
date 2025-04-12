d464434a08120e5e395fdcadbb4c66246388b464
Aniket <aniket.singh.3200.00@gmail.com>
2025-04-12 20:48:51 +0530

    Added new feature to parse units like ByteSize and TimeDuration

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
@@ -152,5 +152,16 @@ public enum TokenType implements Serializable {
    * Represents the enumerated type for the object of type {@code String} with restrictions
    * on characters that can be present in a string.
    */
-  IDENTIFIER
+  IDENTIFIER,
+    /**
+   * Represents the enumerated type for the object of type {@code ByteSize}.
+   * Supports values like 10KB, 5MB, etc.
+   */
+  BYTE_SIZE,
+
+  /**
+   * Represents the enumerated type for the object of type {@code TimeDuration}.
+   * Supports values like 100ms, 2s, 5min, etc.
+   */
+  TIME_DURATION
 }
