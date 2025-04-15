15a04483ee4be4270e38f48827b3832606e75333
Adnanmuhamed <116973450+Adnanmuhamed@users.noreply.github.com>
2025-04-16 00:37:33 +0530

    Update TimeDuration.java

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TimeDuration.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TimeDuration.java
@@ -1,18 +1,4 @@
-/*
- * Copyright © 2017-2019 Cask Data, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+
 
  package io.cdap.wrangler.api.parser;
 
@@ -21,9 +7,7 @@
  import java.util.regex.Matcher;
  import java.util.regex.Pattern;
  
- /**
-  * A {@link Token} that represents a time duration value with units.
-  */
+ 
  public class TimeDuration implements Token {
    private static final Pattern PATTERN = Pattern.compile("(\\d+(?:\\.\\d+)?)([Nn][Ss]|[Mm][Ss]|[Ss])");
    private final long nanoseconds;
