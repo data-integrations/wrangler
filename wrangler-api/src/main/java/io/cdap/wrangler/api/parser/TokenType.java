e44ffd4d762e3aa6d7c7315ca976bf7732e2408d
muhamedAdnan <116973450+Adnanmuhamed@users.noreply.github.com>
2025-04-16 16:17:42 +0530

    Update TokenType with complete documentation

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
@@ -1,54 +1,52 @@
+/**
+ * The TokenType class provides the enumerated types for different types of
+ * tokens that are supported by the grammar.
+ *
+ * Each of the enumerated types specified in this class also has associated
+ * object representing it. e.g. {@code DIRECTIVE_NAME} is represented by the
+ * object {@code DirectiveName}.
+ *
+ * @see Bool
+ * @see BoolList
+ * @see ColumnName
+ * @see ColumnNameList
+ * @see DirectiveName
+ * @see Numeric
+ * @see NumericList
+ * @see Properties
+ * @see Ranges
+ * @see Expression
+ * @see Text
+ * @see TextList
+ * @see ByteSize
+ * @see TimeDuration
+ */
+@PublicEvolving
+public enum TokenType implements Serializable {
+// ... existing code ...
+  /**
+   * Represents the enumerated type for the object of type {@code String} with restrictions
+   * on characters that can be present in a string.
+   */
+  IDENTIFIER,
 
+  /**
+   * Represents the enumerated type for the object of type {@code ByteSize} type.
+   * This type is associated with byte size values with units (KB, MB, GB, TB, PB).
+   * E.g.
+   * <code>
+   *   1KB, 2MB, 3GB, 4TB, 5PB
+   * </code>
+   */
+  BYTE_SIZE,
 
- package io.cdap.wrangler.api.parser;
-
- import io.cdap.wrangler.api.annotations.PublicEvolving;
- 
- import java.io.Serializable;
- 
-
- @PublicEvolving
- public enum TokenType implements Serializable {
-   
-   DIRECTIVE_NAME,
- 
-  
-   COLUMN_NAME,
- 
-   
-   TEXT,
- 
-  
-   NUMERIC,
- 
-   
-   BOOLEAN,
- 
-   
-   COLUMN_NAME_LIST,
- 
-  
-   TEXT_LIST,
- 
-  
-   NUMERIC_LIST,
- 
-  
-   BOOLEAN_LIST,
- 
-  
-   EXPRESSION,
- 
-   
-   PROPERTIES,
- 
-  
-   RANGES,
- 
-  
-   IDENTIFIER,
- 
-   BYTE_SIZE,
-   TIME_DURATION
- }
- 
+  /**
+   * Represents the enumerated type for the object of type {@code TimeDuration} type.
+   * This type is associated with time duration values with units (ns, ms, s).
+   * E.g.
+   * <code>
+   *   1ns, 2ms, 3s
+   * </code>
+   */
+  TIME_DURATION
+}
